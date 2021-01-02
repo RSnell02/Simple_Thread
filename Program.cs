b434f06cb8afd00f3197c057d0f918ea87573448
Ryan <r_snell02@yahoo.com>
2021-01-02 14:06:02 -0500

    First commit. Uploaded Program.cs via GitHub Bash

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Project: SimpleThread
+ * Filename: Program.cs
+ * Author: R. Snell
+ * Date: Jan 02, 2020
+ * Purpose: To demonstrate the use and functioning of a thread.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public const int repetition = 1000;
+        static void Main(string[] args)
+        {
+            ThreadStart threadStart = DoWork;
+            Thread thread = new Thread(threadStart);
+            thread.Start();
+            for (int count = 0; count < repetition; count++)
+                Console.Write('-');
+            thread.Join();
+            Console.WriteLine();
+        }   // end Main()
+
+        public static void DoWork()
+        {
+            for (int count = 0; count < repetition; count++)
+                Console.Write('+');
+        }   // end DoWork()
+    }   // end class
+}   // end namespace
